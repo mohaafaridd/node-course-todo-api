5eedea9d8a3289b51dcfb6418d37dfba0dedad18
Mohammed Farid <moha.khamis@outlook.com>
2019-01-06 15:32:42 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,63 @@
+//const MongoClient = require('mongodb');
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB Server');
+    }
+
+    console.log('Connected to Mongo');
+
+    /* db.collection('Todos')
+
+        .deleteMany({
+            text: "Walk the dog"
+        })
+
+        .then((result) => {
+            console.log(result);
+        }); */
+
+    /* db.collection('Todos')
+
+        .deleteOne({
+            text: "Walk the dog"
+        })
+
+        .then((result) => {
+            console.log(result);
+        });
+
+    db.collection('Todos')
+
+        .findOneAndDelete({
+            completed: false
+        })
+
+        .then((result) => {
+            console.log(result);
+        }); */
+
+    db.collection('Users')
+        .deleteMany({
+            name: "Mohammed"
+        });
+
+    db.collection('Users')
+
+        .findOneAndDelete({
+            _id: new ObjectID("5c31183f70488a07e89b2afd")
+        })
+
+        .then((result) => {
+            console.log(result);
+        });
+
+    //db.close();
+});
