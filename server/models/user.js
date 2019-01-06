9c11477a30d9810146790eeab15a8f3bedb609fa
Mohammed Farid <moha.khamis@outlook.com>
2019-01-06 19:02:58 +0200

    Add POST /todos route

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+})
+
+module.exports = {
+    User
+}
---
+++ server/server.js
@@ -0,0 +1,44 @@
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var express  = require('express');
+var bodyParser = require('body-parser');
+
+/* import * as mongoose from './db/mongoose';
+import * as Todo from './models/todo';
+import * as User from './models/user';
+
+import * as express from 'express';
+import * as bodyParser from 'body-parser'; */
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+
+    console.log(req.body);
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save()
+        .then((doc) => {
+            res.send(doc);
+        }, (e) => {
+            res.status(400).send(e);
+        });
+});
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+})
