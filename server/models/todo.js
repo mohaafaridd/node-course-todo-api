0a7b367be3fce4091d0ddab1306479488fd7d9af
Mohammed Farid <moha.khamis@outlook.com>
2019-01-20 00:37:33 +0200

    Associate users with Todos

--- server/models/todo.js
+++ server/models/todo.js
@@ -16,6 +16,11 @@ var Todo = mongoose.model('Todo', {
     completedAt: {
         type: Number,
         default: null
+    },
+
+    _creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -17,9 +17,10 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
 
     todo.save()
@@ -30,8 +31,8 @@ app.post('/todos', (req, res) => {
         });
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({ _creator: req.user._id }).then((todos) => {
         res.send({
             todos
         })
@@ -40,7 +41,7 @@ app.get('/todos', (req, res) => {
     });
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
@@ -49,7 +50,12 @@ app.get('/todos/:id', (req, res) => {
 
     var msg = 'No todo exists with this ID';
 
-    Todo.findById(id)
+
+
+    Todo.findOne({
+        _id: id,
+        _creator: req.user._id
+    })
         .then((todo) => {
             if (!todo) {
                 return res.status(404).send();
@@ -62,7 +68,7 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     // Get the ID
     var id = req.params.id;
 
@@ -72,7 +78,10 @@ app.delete('/todos/:id', (req, res) => {
     }
 
     // remove todo by ID
-    Todo.findByIdAndRemove(id)
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    })
         .then((todo) => {
             // ERR - no todo by that ID
             if (!todo) {
@@ -88,7 +97,7 @@ app.delete('/todos/:id', (req, res) => {
         })
 })
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
 
@@ -104,11 +113,10 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {
-        $set: body
-    }, {
-            new: true
-        })
+    Todo.findOneAndUpdate({
+        _id: id,
+        _creator: req.user._id
+    }, { $set: body }, { new: true })
         .then((todo) => {
 
             if (!todo) {
