6de10f548c26538bb6875342da76675ef4c680e1
Mohammed Farid <moha.khamis@outlook.com>
2019-01-19 23:00:52 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -79,10 +79,32 @@ UserSchema.statics.findByToken = function (token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({
+        email
+    }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        
+        return new Promise((resolve, reject) => {
+
+            bcrypt.compare(password, user.password, (err, succ) => {
+                if (succ)
+                    resolve(user);
+                else
+                    reject();
+            });
+        })
+    })
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
 
-    if(user.isModified('password')){
+    if (user.isModified('password')) {
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
                 user.password = hash;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var app = express();
 const port = process.env.PORT;
@@ -146,6 +147,18 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 })
 
+// POST /users/login
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
